061145f24f05be331f8fca76fa58b82981c0953b
ParshinSergey <t2transformer@yahoo.com>
2024-01-19 23:47:14 +0300

    changes

--- src/main/java/com/example/selsup/CrptApi.java
+++ src/main/java/com/example/selsup/CrptApi.java
@@ -7,7 +7,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
@@ -16,10 +15,14 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class CrptApi {
+
+    public final static String URI_CREATE_DOCUMENT = "https://ismp.crpt.ru/api/v3/lk/documents/create";
 
     private final TimeUnit timeUnit;
     private final int requestLimit;
@@ -38,16 +41,20 @@ public class CrptApi {
         this.timeStorage = new long[requestLimit];
     }
 
-    public String createDocument(Document document, String token) {
+    public CompletableFuture<HttpResponse<String>> createDocument(Document document, String token) {
 
+        // Пояснение к блоку try:
+        // В "закольцованный" массив размером N (requestLimit) записывается время начиная с которого
+        // будет разрешено выполнение спустя N вызовов.
         lock.lock();
         try {
             long currentTime = System.currentTimeMillis();
-            timeStorage[requestCounter % (requestLimit)] = currentTime;
-            long limitTime = timeStorage[(requestCounter + 1) % (requestLimit)] + timeUnit.toMillis(1);
-            if (currentTime < limitTime) {
-                long sleepTime = limitTime - currentTime;
+            int index = requestCounter % requestLimit;
+            long sleepTime = timeStorage[index] - currentTime;
+            timeStorage[index] = currentTime + timeUnit.toMillis(1);
+            if (sleepTime > 0) {
                 try {
+                    timeStorage[index] += sleepTime;
                     TimeUnit.MILLISECONDS.sleep(sleepTime);
                 } catch (InterruptedException e) {
                     Thread.currentThread().interrupt();
@@ -59,37 +66,26 @@ public class CrptApi {
             lock.unlock();
         }
 
-        String bodyRequest;
-        try {
-            bodyRequest = objectToJson(document);
-        } catch (JsonProcessingException e) {
-            return "Error processing (parsing, generating) JSON content " + e.getMessage();
-        }
         HttpRequest httpRequest = HttpRequest.newBuilder()
-                .uri(URI.create("https://ismp.crpt.ru/api/v3/lk/documents/create"))
-                .POST(HttpRequest.BodyPublishers.ofString(bodyRequest))
+                .uri(URI.create(URI_CREATE_DOCUMENT))
+                .POST(HttpRequest.BodyPublishers.ofString(objectToJson(document)))
                 .header("Content-Type", "application/json")
                 .header("Authorization", "Bearer " + token)
                 .build();
 
-        HttpResponse<String> httpResponse;
-        try {
-            httpResponse = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
-        } catch (IOException e) {
-            return "Error connection to URL " + e.getMessage();
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
-            return "Error connection to URL " + e.getMessage();
-        }
-
-        return httpResponse.body();
+        return httpClient.sendAsync(httpRequest, HttpResponse.BodyHandlers.ofString());
     }
 
-    public static String objectToJson(Object obj) throws JsonProcessingException {
+
+    public static String objectToJson(Object obj)  {
         ObjectMapper objectMapper = new ObjectMapper().registerModule(new JavaTimeModule());
         DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
         objectMapper.setDateFormat(df);
-        return objectMapper.writeValueAsString(obj);
+        try {
+            return objectMapper.writeValueAsString(obj);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 
@@ -127,6 +123,7 @@ public class CrptApi {
         String uitu_code;
     }
 
+
     @Getter
     @Setter
     @NoArgsConstructor
@@ -134,14 +131,15 @@ public class CrptApi {
         private String participantInn;
     }
 
-    public static void main(String[] args) throws InterruptedException {
+
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
         CrptApi crptApi = new CrptApi(TimeUnit.SECONDS, 4);
-        for (int i = 0; i < 12; i++){
+        for (int i = 0; i < 15; i++){
             var document = new CrptApi.Document();
             document.setProduction_date(LocalDate.now());
-            document.setDoc_id("12345");
-            System.out.println(crptApi.createDocument(document, "token"));
-            Thread.sleep(600);
+            document.setDoc_id("Number " + i);
+            System.out.println(crptApi.createDocument(document, "token").get().statusCode());
+            Thread.sleep(100);
         }
     }
 
